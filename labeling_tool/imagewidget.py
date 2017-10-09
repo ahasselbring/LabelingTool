899ef9678b5e6732883565be51be139eded7f440
Arne Hasselbring <arne.hasselbring@tu-harburg.de>
2017-10-09 12:12:03 +0200

    Use cross cursor for image widget

--- labeling_tool/imagewidget.py
+++ labeling_tool/imagewidget.py
@@ -14,6 +14,7 @@ class ImageWidget(QWidget):
         self.__selectedImage = None
 
         self.setMouseTracking(True)
+        self.setCursor(Qt.CrossCursor)
 
     def mousePressEvent(self, event):
         super().mousePressEvent(event)
